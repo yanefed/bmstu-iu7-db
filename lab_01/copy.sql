29ed7e989d70bbf446b5766082cc4194f64d6235
Ivan Bogatyrev <yaneeee@vk.com>
2024-09-17 20:50:12 +0300

    fixed serials after data copy

--- lab_01/copy.sql
+++ lab_01/copy.sql
@@ -22,3 +22,25 @@ COPY rehearsals_in_room (id, rehearsal_id, room_id, status)
     FROM '/tmp/rehearsal_rooms.csv'
     DELIMITER ','
     CSV HEADER;
+
+
+SELECT setval(pg_get_serial_sequence('postgres.public.rehearsal', 'rehearsal_id'), COALESCE(MAX(rehearsal_id), 1) + 1,
+              false)
+FROM postgres.public.rehearsal;
+
+SELECT setval(pg_get_serial_sequence('postgres.public.customer', 'customer_id'), COALESCE(MAX(customer_id), 1) + 1,
+              false)
+FROM postgres.public.customer;
+
+SELECT setval(pg_get_serial_sequence('postgres.public.room', 'room_id'), COALESCE(MAX(room_id), 1) + 1,
+              false)
+FROM postgres.public.room;
+
+SELECT setval(pg_get_serial_sequence('postgres.public.hour', 'hour_id'), COALESCE(MAX(hour_id), 1) + 1,
+              false)
+FROM postgres.public.hour;
+
+SELECT setval(pg_get_serial_sequence('postgres.public.rehearsals_in_room', 'id'), COALESCE(MAX(id), 1) + 1,
+              false)
+FROM postgres.public.rehearsals_in_room;
+
